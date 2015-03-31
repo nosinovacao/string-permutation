7f679501c49b15352e5389a8fc79af2bd6132a68
unknown <mmarques@DEV-CAT-01.zondev.lab>
2015-03-31 18:44:31 +0100

    package.json

---
+++ permutation.js
@@ -0,0 +1,49 @@
+﻿"use strict";
+ 
+
+exports.PermuteName = function (completeName) {
+    
+    completeName = completeName.replace(/\s{2,}/g, ' ').trim();
+    
+    if (!completeName || completeName.length <= 0)
+        return;
+
+    var words = completeName.split(' ');
+    var result = [];
+    
+    if (words.length == 1) {
+        result.push(completeName);
+        return result;
+    }
+    
+    for (var curPos = 0; curPos < words.length - 1; curPos++) {
+        
+        var aux = curPos + 1;
+        
+        while (aux <= words.length - 1) {
+            
+            var aux2 = aux;
+            var lastNames = '';
+            while (aux2 <= words.length - 1) {
+                if ((words.length - 1) != aux) {
+                    lastNames += words[aux2] + ' ';
+                }
+                aux2 = aux2 + 1;
+            }
+            if (lastNames)
+                result.push(words[curPos] + ' ' + lastNames.trim());
+            
+            result.push(words[curPos] + ' ' + words[aux]);
+            
+            aux = aux + 1;
+        }
+    }
+    
+    if (words.length > 3) {
+        for (var i = 2; i <= words.length - 1; i++) {
+            result.push(completeName.replace(' ' + words[i], ''));
+        }
+    }
+
+    return result;
+}
