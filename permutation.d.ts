c637d61b75c6ef5c33bf91bf6554460ef3048d88
Mário Marques <mariomarques@boldint.com>
2015-10-06 13:52:01 +0100

    add d.ts typing file

---
+++ permutation.d.ts
@@ -0,0 +1,18 @@
+﻿declare module "permutation" {
+
+    /**
+    * Permutes words present on a string
+    *
+    * @param completeName string containing name to be permuted
+    * @return {Array<string>} Array that contains permuted names
+    */
+    function permuteName(completeName: string): Array<string>;
+
+    /**
+    * Permutes words present on an Array
+    *
+    * @param sourceArray Array containing names to be permuted
+    * @return {Array<string>} Array that contains permuted names
+    */
+    function permuteArray(sourceArray: Array<string>): Array<string>;
+}
